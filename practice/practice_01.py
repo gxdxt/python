e8170580c779f1be6de04ea46a062d5d550eaf7e
yoosoony <ystar819@naver.com>
2021-08-18 00:40:04 +0900

    Decide the step size

--- practice/practice_01.py
+++ practice/practice_01.py
@@ -6,3 +6,5 @@ print(myString[-1])
 print(myString[2:])
 print(myString[:3]) #Up to but not including!!
 print(myString[::]) #To see the all the way from the beginning to the end
+#the third place is for the step size, I can decide the size of jump point
+print(myString[::2])
