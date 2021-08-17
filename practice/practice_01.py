213b11e151afcdef14b0706e6c49d52b46b6f9b7
yoosoony <ystar819@naver.com>
2021-08-18 00:37:36 +0900

    Not slice the string

--- practice/practice_01.py
+++ practice/practice_01.py
@@ -5,3 +5,4 @@ print(myString[0])
 print(myString[-1])
 print(myString[2:])
 print(myString[:3]) #Up to but not including!!
+print(myString[::]) #To see the all the way from the beginning to the end
