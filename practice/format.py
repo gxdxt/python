30540f0cb9010ed377a0bc9356b769c5e4927850
yoosoony <ystar819@naver.com>
2021-08-20 17:51:17 +0900

    Change the order of format method

--- practice/format.py
+++ practice/format.py
@@ -3,4 +3,5 @@
 
 # curly braces
 print('This is for the test of {}'.format('format()'))
-print('First typing with happy hacking {} {} {} '.format('is', 'feeling', 'good'))
+print('First typing with happy hacking {1} {0} {2} '.format('is', 'feeling', 'good'))
+# format의 순서를 내가 지정할 수 있다.
