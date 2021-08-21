2a6030af864ba8b60d1efeaf228fe84b22c05db5
yoosoony <ystar819@naver.com>
2021-08-21 12:34:51 +0900

    Put exceptions and headers for solving the requests.get error

--- practice/nheel_proj/nheel_proj.py
+++ practice/nheel_proj/nheel_proj.py
@@ -4,7 +4,17 @@ from bs4 import BeautifulSoup
 url = 'https://www.coupang.com/vp/products/237181694?itemId=751734793&vendorItemId=4898821183&q=%EC%83%A4%EB%B0%94%EC%8A%A4&itemsCount=36&searchId=ccd540a4610044b3b49704aebcfbf4f0&rank=34&isAddedCart='
 print("play")
 # response가 안될 때가 있고, 될 때가 있다. 그 이유 분석 필요
-response = requests.get(url)
+try:
+    headers = { "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36" }
+    response = requests.get(url, headers = headers)
+except requests.exceptions.Timeout as errd:
+    print("Timeout error : ", errd)
+except requests.exceptions.ConnectionError as errd:
+    print("Connection error : ", errd)
+except requests.exceptions.HTTPError as errd:
+    print("HTTP error : ", errd)
+except requests.exceptions.RequestException as errd:
+    print("Exception : ", errd)
 print("response passed")
 
 
