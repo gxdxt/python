aa69f1f03da2904c26c1dfeca7d44ae8b3bb8dc5
yoosoony <ystar819@naver.com>
2021-08-22 01:30:19 +0900

    Test the func nheel

--- practice/nheel_proj/nheel_proj_01.py
+++ practice/nheel_proj/nheel_proj_01.py
@@ -19,6 +19,23 @@ def nheelProj(headers, url, price):
         soup = BeautifulSoup(html, 'html.parser')
         title = soup.select_one(
             'div.prod-price-container > div.prod-price > div.prod-price-onetime > div.prod-sale-price > span.total-price > strong')
-        print(title.get_text().split('원'))
+
+        coupang_price = title.get_text().split('원')[0]
+        coupang_price = coupang_price.replace(',', "")
+        coupang_price = int(coupang_price)
+        original_price = int(price)
+        if (coupang_price > original_price) :
+            print
+            flag = False
+        else :
+            flag = True
+
+        if flag:
+            return print('문제 없습니다.')
+        else:
+            return print('쿠팡가가 너무 높습니다.')
+
     else:
         print(response.status_code)
+
+nheelProj({ "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36" }, 'https://www.coupang.com/vp/products/86224761?itemId=271802745&vendorItemId=3657804369&sourceType=srp_product_ads&clickEventId=23216e90-c4e7-4b5f-9cac-fcf74ce013ab&korePlacement=15&koreSubPlacement=1&q=%EC%83%A4%EB%B0%94%EC%8A%A4&itemsCount=36&searchId=ccd540a4610044b3b49704aebcfbf4f0&rank=0&isAddedCart=', 1000)
