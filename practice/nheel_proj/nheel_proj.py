6041156d3bbfc8fa1ab0aaa25844032795298ed4
yoosoony <ystar819@naver.com>
2021-08-21 12:37:41 +0900

    Comment for the User-Agent

--- practice/nheel_proj/nheel_proj.py
+++ practice/nheel_proj/nheel_proj.py
@@ -1,9 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.coupang.com/vp/products/237181694?itemId=751734793&vendorItemId=4898821183&q=%EC%83%A4%EB%B0%94%EC%8A%A4&itemsCount=36&searchId=ccd540a4610044b3b49704aebcfbf4f0&rank=34&isAddedCart='
+url = 'https://www.coupang.com/vp/products/86224761?itemId=271802745&vendorItemId=3657804369&sourceType=srp_product_ads&clickEventId=23216e90-c4e7-4b5f-9cac-fcf74ce013ab&korePlacement=15&koreSubPlacement=1&q=%EC%83%A4%EB%B0%94%EC%8A%A4&itemsCount=36&searchId=ccd540a4610044b3b49704aebcfbf4f0&rank=0&isAddedCart='
 print("play")
 # response가 안될 때가 있고, 될 때가 있다. 그 이유 분석 필요
+# User-Agent에 대한 정보를 넣고있지 않아서야
+# 그럼 User-Agent가 어떠한 의미가 있는데??
 try:
     headers = { "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36" }
     response = requests.get(url, headers = headers)
