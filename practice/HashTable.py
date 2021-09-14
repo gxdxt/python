d4554cea8acdac5a892226fa746fc7c42fe623a3
yoosoony <ystar819@naver.com>
2021-09-15 07:32:10 +0900

    Add the hashing function and ord func

--- practice/HashTable.py
+++ practice/HashTable.py
@@ -1,2 +1,13 @@
 hash_table = list(['O' for i in range(10)])
 print(hash_table)
+# list comprehension
+
+# 간단한 hashing function
+def hash_func(key):
+    return key % 5
+
+## ord(): 문자의 ASCII 코드를 출력하는 것
+data1 = 'James'
+data2 = 'Dave'
+data3 = 'Trump'
+print(ord(data1[0]), ord(data2[0]), ord(data3[0]))
