79e4cc3cec030b2bb1b423c9a0388cabbd6c8ac3
yoosoony <ystar819@naver.com>
2021-09-16 00:06:18 +0900

    Practice making the Linked List from the scratch

---
+++ practice/LinkedList3_V0.py
@@ -0,0 +1,18 @@
+
+# 각각의 노드들이 연결되면서 결국 Linked List라는 구조가 생성되는 것이다. 이렇게 안에 들어있는 값은 다르지만, 그 형태 및 구조는 동일한 'Node'라는 것을 정의한 부분
+# 즉, Node라는 값을 찍어낼 '거푸집'을 만든 것.
+class Node:
+
+    # __init__의 역할은 초기값을 선언하는 것이다.
+    # Node가 호출될 때 항상 자동으로 실행되는 함수라고 생각하면 되는 것 같다.
+    # 물론, setValue(self, data, prev, next)라는 함수를 만들고, 해당 클래스를 호출할 때 마다 setValue 함수도 호출해줘도 된다.
+    # 근데 굳이?
+    def __init__(self, data, prev=None, next=None):
+        # self의 의미는 각각 인스턴스에서 활용하게 되는 부분이다.
+        # 이 객체에서는 인스턴스를 위한 틀을 만들어 주는 것이다.
+        # 여기서 지정한 self 값은 해당 객체가 인스턴스가 될 때 해당 인스턴스를 의미하는 인자가 된다.
+        self.data = data
+        self.prev = prev
+        self.next = next
+
+
