b6669e688ddabfbda1874afd4f058468379f4888
yoosoony <ystar819@naver.com>
2021-09-25 07:20:19 +0900

    Fix the line which is located wrongly

--- practice/nheel_proj/RR_proj.py
+++ practice/nheel_proj/RR_proj.py
@@ -1,5 +1,10 @@
 import random
+import threading
 
+
+def startTimer():
+    timer = threading.Timer(5, startTimer)
+    timer.start()
 # 사용자는 최소 2명 이상
 # input으로 사용자 이름을 정의한다.
 
@@ -15,6 +20,7 @@ while Participant != "end" :
 # Game type에 대한 설명
 print('Game type에 대한 설명.')
 print('1번 게임 타입: 실린더를 최초 1회만 돌리는 경우(6 차례 내로 게임이 종료됩니다.)')
+# 시간 타임아웃 걸자
 print('2번 게임 타입: 실린더를 매 차례마다 돌리는 경우(게임이 오랫동안 지속될 수 있습니다.)')
 # 입력된 input의 length를 확인하여 게임에 참여하는 인원을 가늠한다.
 GameType = input('원하시는 Game type을 입력해주세요. : (기본값 = 1)')
@@ -25,7 +31,9 @@ if GameType != 1 or GameType != 2 :
 if GameType == 1 or GameType == '':
     for index in range(len(Participants)):
         print(Participants[index] + '의 차례입니다.')
+        startTimer()
         print('실린더를 한 번만 돌리게 됩니다.')
+        startTimer()
         spin = input('엔터를 누르면, 실린더를 돌립니다.')
         if spin == '\n' :
             # 랜덤으로 총알을 장전합니다.
@@ -36,7 +44,7 @@ if GameType == 1 or GameType == '':
             # 일단 처음에는 랜덤으로 숫자 선택
             reload = input('엔터를 누르면, 장전을 합니다.')
             if reload == '\n' :
-            pick = random.randint(0, 5)
+                pick = random.randint(0, 5)
             if target == pick:
                 print("BANG!")
                 print(Participants[index] + '님이 사망하셨습니다.')
