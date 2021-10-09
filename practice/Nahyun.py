591a9b0e0f5de850e50b875cb4f562126b430f33
yoosoony <ystar819@naver.com>
2021-10-09 23:35:38 +0900

    Practice with RG

---
+++ practice/Nahyun.py
@@ -0,0 +1,22 @@
+import random
+
+list = []
+num = 0
+while len(list) < 100:
+    list.append(num)
+    num = round(random.random()*1000)
+
+print(list)
+
+
+# 이분 탐색  binary search
+# 1 - 100 까지 중에 99
+# 1 - 100 까지 중에 99
+# 1 - 100 중 딱 반 50
+# 51 - 100 중 딱 반 75
+# 75 - 100 중 딱 반 87
+
+
+
+
+
