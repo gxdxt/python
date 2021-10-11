4cbd417df97c8b7978af70463499e1cde65be583
yoosoony <ystar819@naver.com>
2021-10-11 15:19:18 +0900

    Add the code that compares the value of inserted and the parent

--- practice/Heap_V0.py
+++ practice/Heap_V0.py
@@ -21,6 +21,15 @@ class Heap:
 
     # 삽입하는 데이터의 크기를 이전 노드와 비교하여 위치를 변경해야하는 지 확인하는 함수 >> True이면 위치 변경
     def move_up(self, inserted_idx):
+        # 삽입하는 노드가 루트 노드일 경우에는 할 필요 없다.
+        if inserted_idx <= 1:
+            return False
+
+        parent_idx = inserted_idx // 2
+        if self.heap_array[parent_idx] < self.heap_array[inserted_idx]:
+            return True
+        else:
+            return False
 
 
     def insert(self, data):
@@ -31,7 +40,6 @@ class Heap:
 
         self.heap_array.append(data)
         # 삽입하는 데이터가 루트 노드가 아닐 경우, 데이터 위치 변경의 코드들이 필요하다.
-            
 
         # 지금 들어가는 데이터의 index 번호를 알아야 한다.
         inserted_idx = len(self.heap_array) - 1
@@ -40,6 +48,16 @@ class Heap:
             parent_idx = inserted_idx // 2 # /면 소수점까지 모두 나오고, //하면 몫 자연수만 나온다.
             # 두 자리를 변경한다.
             self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx] # swap
+            inserted_idx = parent_idx
+            # Q. 그럼 기존 parent_idx는 inserted_idx로 변경 안해줘도 되나?
+            # A. 해당 idx 값은 실제 값이 아니라 자체적으로 부여한 임시 값이기 때문에 상관 없다.
 
         return True
 
+heap = Heap(15)
+heap.insert(10)
+heap.insert(8)
+heap.insert(5)
+heap.insert(4)
+heap.insert(20)
+print(heap.heap_array)
