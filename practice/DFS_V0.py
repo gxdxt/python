52c34ab14122a45794de79f804de2b029c4391b3
yoosoony <ystar819@naver.com>
2021-10-21 12:31:40 +0900

    Add the DFS algorithm

---
+++ practice/DFS_V0.py
@@ -0,0 +1,26 @@
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop()
+        if node not in visited:
+            visited.append(node)
+            # 해당 노드와 연결된 노드들을 뿌려주는 method
+            need_visit.extend(graph[node])
+
+    return visited
+
+graph = dict()
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+print(dfs(graph, 'A'))
