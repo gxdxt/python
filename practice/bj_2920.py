5a962d1139cb21c57eed692f814ded0592793c9d
yoosoony <ystar819@naver.com>
2021-11-13 01:42:40 +0900

    Solve the baekjoon problem num 2920

---
+++ practice/bj_2920.py
@@ -0,0 +1,20 @@
+#다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다. 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어 표현한다. c는 1로, d는 2로, ..., C를 8로 바꾼다.
+
+#1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면 mixed 이다.
+
+#연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을 작성하시오.
+
+def solve(music):
+    # 리스트 '순서'가 중요한 거니까
+    answer = ""
+    gap = 0
+    for i in range(len(music)-1):
+        gap *= (music[i] - music[i+1])
+    if gap == 1:
+        answer = 'descending'
+    elif gap == -1:
+        answer = 'ascending'
+    else:
+        answer = 'mixed'
+
+# 8:10
